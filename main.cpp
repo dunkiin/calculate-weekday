cd91dca0d202dc0973a3fdc94f2db3c6fcb9b608
Alfie Sun <alfiesunn666@gmail.com>
2023-10-08 23:27:59 -0700

    Updated the output message.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include "TextInput.h"
 #include "Weekday.h"
-
-
 #include <iostream>
 #include <string>
 
+using namespace std;
 
 
 int main() {
@@ -17,6 +16,9 @@ int main() {
 
 	//std::cout << year << std::endl << month << std::endl << dayOfMonth << std::endl;
 
+	std::cout << "The Year you enter is: " << year << std::endl;
+	std::cout << "The Month you enter is: " << month << std::endl;
+	std::cout << "The Day you enter is: " << dayOfMonth << std::endl;
 	std::cout << "Day of the week is " << getWeekdayName(days);
 
 	return 0;
